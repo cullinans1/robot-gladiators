19616e8b3e101ca9e8765f99199351d6aa8482d8
cullinans1  <sammy.cullinan@gmail.com>
2020-06-09 15:36:25 -0700

    added round number alert at the beginning of each round

--- assets/js/game.js
+++ assets/js/game.js
@@ -67,6 +67,12 @@ var fight = function(enemyName) {
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+    }
+    else {
+        window.alert("You have lost your robot in battle! Game OVER!");
+    }
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
     fight(enemyNames[i]);
